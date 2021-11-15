b4885b363d11116192a23a6a57ef4292316c7ebb
chang_jian <chang_jian@hoperun.com>
2021-11-14 20:51:24 -0500

    加入mingw兼容文件
    Signed-off-by: chang_jian <chang_jian@hoperun.com>

---
+++ include/mingw/ucontext.h
@@ -0,0 +1,11 @@
+// This is an incomplete & imprecice implementation of the *nix file
+// by the same name
+#ifndef __MINGW_UCONTEXT__
+#define __MINGW_UCONTEXT__
+
+#ifdef MINGW // Only for cross compilation to mingw
+
+#include <sys/ucontext.h>
+
+#endif // MINGW
+#endif
