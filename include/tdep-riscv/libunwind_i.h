83b2d09cae8484dd30712d4753be5981de415ccb
wenfei6316 <wenfei6316@163.com>
2023-12-21 16:51:27 +0800

    fixed 0f69de5 from https://gitee.com/wenfei6316/third_party_libunwind_qemu/pulls/124 add riscv64 arch compiler
    Signed-off-by: wenfei6316 <wenfei6316@163.com>
    Change-Id: Ied06be5c90c6114d4c8ae4c49dd2113c7cd41561

--- include/tdep-riscv/libunwind_i.h
+++ include/tdep-riscv/libunwind_i.h
@@ -44,6 +44,8 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.  */
 # error "Unsupported address size"
 #endif
 
+/* Add For Cache MAP And ELF */
+#include "map_info.h"
 #include "mempool.h"
 #include "dwarf.h"
 
@@ -66,9 +68,23 @@ struct unw_addr_space
     unw_word_t dyn_info_list_addr;      /* (cached) dyn_info_list_addr */
     struct dwarf_rs_cache global_cache;
     struct unw_debug_frame_list *debug_frames;
+    /* Add For Cache MAP And ELF */
+    struct map_info *map_list;
+    struct cursor *cursor;
+    int pid;
 };
 
 #define tdep_big_endian(as)             ((as)->big_endian)
+
+static inline struct cursor *
+get_cursor_from_as(unw_addr_space_t as)
+{
+  if (as->cursor) {
+    return (struct cursor *)(as->cursor);
+  }
+
+  return NULL;
+}
 
 struct cursor
   {
@@ -161,7 +177,6 @@ dwarf_getfp (struct dwarf_cursor *c, dwarf_loc_t loc, unw_fpreg_t *val)
 {
   char *valp = (char *) &val;
   unw_word_t addr;
-  int ret;
 
   if (DWARF_IS_NULL_LOC (loc))
     return -UNW_EBADREG;
@@ -185,7 +200,6 @@ dwarf_putfp (struct dwarf_cursor *c, dwarf_loc_t loc, unw_fpreg_t val)
 {
   char *valp = (char *) &val;
   unw_word_t addr;
-  int ret;
 
   if (DWARF_IS_NULL_LOC (loc))
     return -UNW_EBADREG;
@@ -291,9 +305,10 @@ extern int tdep_search_unwind_table (unw_addr_space_t as, unw_word_t ip,
                                      unw_dyn_info_t *di, unw_proc_info_t *pi,
                                      int need_unwind_info, void *arg);
 extern void *tdep_uc_addr (ucontext_t *uc, int reg);
-extern int tdep_get_elf_image (struct elf_image *ei, pid_t pid, unw_word_t ip,
-                               unsigned long *segbase, unsigned long *mapoff,
-                               char *path, size_t pathlen);
+/* Add For Cache MAP And ELF */
+extern struct map_info *tdep_get_elf_image (unw_addr_space_t as, pid_t pid,
+                                            unw_word_t ip);
+
 extern void tdep_get_exe_image_path (char *path);
 extern int tdep_access_reg (struct cursor *c, unw_regnum_t reg,
                             unw_word_t *valp, int write);
